8e56bb7e4119dd66705fb69c6e84dc99a88f7203
Vilcu Iustin <vilcuiustin@gmail.com>
2022-02-02 23:46:45 +0200

    Repair Workspace

--- Models/Workspace.cs
+++ Models/Workspace.cs
@@ -5,6 +5,7 @@ namespace SGBD_Project.Models
 {
     public class Workspace : BaseEntity
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public Guid UserId { get; set; }
         public User Owner { get; set; }
